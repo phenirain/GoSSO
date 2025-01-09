aaac8111c5092cb5c96dfc2ad34ff5e4082d79cd
Dmitrii Kirilov <144320453+EtoNeAnanasbI95@users.noreply.github.com>
2025-01-09 20:38:14 +0300

    feat: add user model

---
+++ internal/domain/models/User.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	Id           int32  `json:"id" db:"id"`
+	Name         string `json:"name" db:"name"`
+	Email        string `json:"email" db:"email"`
+	PasswordHash []byte `json:"password_hash" db:"password_hash"`
+}
