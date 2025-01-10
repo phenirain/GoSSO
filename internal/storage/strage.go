3316f7fcd034b4c7382c35700fbe7bc2cce07312
Dmitrii Kirilov <144320453+EtoNeAnanasbI95@users.noreply.github.com>
2025-01-10 16:32:29 +0300

    feat: write storage init package

--- internal/storage/strage.go
+++ internal/storage/strage.go
@@ -1,1 +1,15 @@
 package storage
+
+import "github.com/jmoiron/sqlx"
+
+func MustInitDb(cs string) *sqlx.DB {
+	db, err := sqlx.Connect("postgres", cs)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
