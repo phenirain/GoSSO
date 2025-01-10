958640acf24d64bcc2c1e4c158d7c491af7182e9
Dmitrii Kirilov <144320453+EtoNeAnanasbI95@users.noreply.github.com>
2025-01-10 16:32:04 +0300

    refator: change user model

--- internal/domain/models/User.go
+++ internal/domain/models/User.go
@@ -3,6 +3,6 @@ package models
 type User struct {
 	Id           int64  `json:"id" db:"id"`
 	Name         string `json:"name" db:"name"`
-	Email        string `json:"email" db:"email"`
+	Email        string `json:"email" db:"email" binding:"required"`
 	PasswordHash []byte `json:"password_hash" db:"password_hash"`
 }
